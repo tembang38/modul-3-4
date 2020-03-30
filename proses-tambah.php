1c798bf7a21c2f3ceed6069ec4b02bd090d9ba05
tembang38 <60780862+tembang38@users.noreply.github.com>
2020-03-30 20:46:44 +0700

    Add files via upload

--- proses-tambah.php
+++ proses-tambah.php
@@ -4,26 +4,27 @@ include '../koneksi.php';
 
 if(isset($_POST['simpan']))
 {
-    $judul = $_POST['judul'];
-    $stok = $_POST['stok'];
-    $telp = $_POST['telp'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $id_level = 3;
-    $sql = "INSERT INTO buku (judul, stok, telp, username, password, id_level)
-            VALUES ('$judul', '$stok', '$telp', '$username', '$password', $id_level)";
-    
-    $res = mysqli_query($koneksi, $sql);
+$nama = $_POST['nama'];
+$kelas = $_POST['kelas'];
+$telp = $_POST['telp'];
+$username = $_POST['username']; 
+$password = $_POST['password']; 
+$id_level = 3; 
+$sql = "INSERT INTO anggota (nama, kelas, telp, username, password, id_level)            
+        VALUES ('$nama', '$kelas', '$telp', '$username', '$password', $id_level)"; 
 
-    $count = mysqli_affected_rows($koneksi);
+$res = mysqli_query($koneksi, $sql);
+
+$count = mysqli_affected_rows($koneksi);
 
 if($count == 1)
 {
     header("Location: index.php");
- }
- else 
- {
-    header("Location: tambah.php");
- }
 }
+}
+else
+{
+    header("Location:tambah.php");
+}
+
 ?>
